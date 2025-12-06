80a41a3fc98855afac92f93d52ed80f4bf1aa0a6
AkshatGupta-30 <akshatgupta0030@gmail.com>
2025-12-06 23:59:31 +0530

    feat: enhance ui

--- src/components/BookmarkFolder.tsx
+++ src/components/BookmarkFolder.tsx
@@ -15,12 +15,12 @@ const BookmarkFolder = ({ bookmark }: { bookmark: BookmarkFolderType }) => {
     <button
       onClick={() => addBreadcrumb(bookmark)}
       onContextMenu={(e) => handleContextMenu(e, bookmark)}
-      className="gap-auto relative flex w-full cursor-pointer flex-col items-center justify-start transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-lg"
+      className="group gap-auto relative flex w-full cursor-pointer flex-col items-center justify-start transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-lg"
     >
       <div className="mb-3 flex h-15 w-15 items-center justify-center rounded-full bg-black transition-transform duration-300 group-hover:scale-110 dark:bg-white">
         <FaFolder className="h-9 w-9 text-white dark:text-black" />
       </div>
-      <span className="text-center text-sm font-medium text-gray-700 transition-colors group-hover:text-black dark:text-gray-300 dark:group-hover:text-white">
+      <span className="text-center text-sm font-medium text-gray-700 transition-colors group-hover:text-black dark:text-gray-300 group-hover:dark:text-white">
         {truncateText(title, 25)}
       </span>
     </button>
--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -19,13 +19,13 @@ const Site = ({ site, topSite: isTopSite }: ISiteProps) => {
     <a
       href={url}
       onContextMenu={(e) => !isTopSite && handleContextMenu(e, site)}
-      className="gap-auto relative flex w-full cursor-pointer flex-col items-center justify-start transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-lg"
+      className="group gap-auto relative flex w-full cursor-pointer flex-col items-center justify-start transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-lg"
     >
       <div className="mb-3 flex min-h-15 min-w-15 items-center justify-center rounded-lg bg-black/10 transition-transform duration-300 group-hover:scale-110 dark:bg-white/10">
         <img
           src={faviconUrl}
           alt={title}
-          className="h-9 w-9 object-contain opacity-80 transition-opacity group-hover:opacity-100"
+          className={`h-9 w-9 object-contain opacity-80 transition-all duration-150 group-hover:opacity-100 ${isTopSite ? "group-hover:-rotate-6 group-hover:scale-105": ""}`}
         />
       </div>
       <span className="line-clamp-2 text-center text-sm font-medium text-gray-700 transition-colors group-hover:text-black dark:text-gray-300 group-hover:dark:text-white">
