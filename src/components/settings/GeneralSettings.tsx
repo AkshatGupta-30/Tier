66c2594e16465004c69cf1272fc2d02876797d1b
AkshatGupta-30 <akshatgupta0030@gmail.com>
2025-12-12 22:09:04 +0530

    feat: Add hide quote switch

--- src/components/settings/GeneralSettings.tsx
+++ src/components/settings/GeneralSettings.tsx
@@ -2,14 +2,25 @@ import { memo } from 'react';
 
 import SearchEngineSelector from '@components/SearchEngineSelector';
 import SettingsSection from '@components/settings/SettingsSection';
+import Switch from '@components/Switch';
+import useTheme from '@hooks/useTheme';
 
 const GeneralSettings = () => {
+  const { hideQuote, toggleHideQuote } = useTheme();
+
   return (
     <div className="animate-in fade-in slide-in-from-right-4 flex flex-col gap-8 duration-300">
       <SettingsSection
         title="Search Engine"
         component={<SearchEngineSelector />}
       />
+      <div className="flex flex-row justify-between">
+        <p className="text-lg font-bold text-gray-900 dark:text-gray-100">Hide Today's Quote</p>
+        <Switch
+          checked={hideQuote}
+          onChange={toggleHideQuote}
+        />
+      </div>
     </div>
   );
 };
