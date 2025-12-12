c0ab623acd91dfe42421f81fda7e9f21175df2c2
AkshatGupta-30 <akshatgupta0030@gmail.com>
2025-12-12 21:48:32 +0530

    fix: remove modal from new tab preview

--- src/components/NewTabPreview.tsx
+++ src/components/NewTabPreview.tsx
@@ -1,43 +1,27 @@
-import { Activity, useEffect, useRef, useState, type SetStateAction, type Dispatch } from 'react';
-import { createPortal } from 'react-dom';
+import { Activity } from 'react';
 
 import NewTab from '@pages/NewTab';
-import useTheme from '@hooks/useTheme';
-import { IoMdClose } from 'react-icons/io';
-
-const modalRoot = document.querySelector('#modal-root') as Element;
 
 interface NewTabPreviewProps {
-  setVisible: Dispatch<SetStateAction<boolean>>;
-  isOpen?: boolean;
+  open?: boolean;
 }
 
-const NewTabPreview = ({ isOpen, setVisible }: NewTabPreviewProps) => {
-  const { backgroundColor } = useTheme();
-
-  return createPortal(
+const NewTabPreview = ({ open: isOpen }: NewTabPreviewProps) => {
+  return (
     <Activity mode={isOpen ? 'visible' : 'hidden'}>
-      <div className={`fixed inset-0 z-50`}>
-        <button className="fixed inset-0 z-0 bg-black/80" />
+      <div
+        className={`relative z-1 flex h-full items-center justify-center overflow-hidden rounded-xl border border-black dark:border-white`}
+      >
         <div
-          className={`relative z-1 flex h-full w-full scale-90 items-center justify-center rounded-2xl ${backgroundColor.classes}`}
+          className="h-full w-fit origin-top-left overflow-hidden"
+          inert
         >
-          <div
-            className="h-full w-full px-10"
-            inert
-          >
+          <div className="origin-top-left">
             <NewTab />
           </div>
-          <button
-            className="absolute top-0 right-0 z-1000 m-3 cursor-pointer rounded-full bg-black/40 p-2 transition-colors duration-300 hover:bg-black/80 dark:bg-white/40 dark:hover:bg-white/80"
-            onClick={() => setVisible(false)}
-          >
-            <IoMdClose className="text-4xl text-white dark:text-black" />
-          </button>
         </div>
       </div>
-    </Activity>,
-    modalRoot,
+    </Activity>
   );
 };
 
